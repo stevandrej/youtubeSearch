04dae118dcbc1ab3ec35834875c30423fa8cb007
Andrej Stevkovski <stevandrej@yahoo.com>
2020-01-08 17:37:56 +0100

    Updates

---
+++ precache-manifest.97914bc7c51ed064f213d285cc95faf8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f9e3b3b5a9d5c1fadcec9e98e23e925",
+    "url": "/youtubeSearch/index.html"
+  },
+  {
+    "revision": "110fce31e59a4fba67c5",
+    "url": "/youtubeSearch/static/css/main.9c050a18.chunk.css"
+  },
+  {
+    "revision": "8d8c7d2a5e6d171af7c6",
+    "url": "/youtubeSearch/static/js/2.8ef0b23c.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/youtubeSearch/static/js/2.8ef0b23c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "110fce31e59a4fba67c5",
+    "url": "/youtubeSearch/static/js/main.1553fe58.chunk.js"
+  },
+  {
+    "revision": "e5c6391fae9e9d181c7f",
+    "url": "/youtubeSearch/static/js/runtime-main.9b68bfb7.js"
+  }
+]);
